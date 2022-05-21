cfebfe7d274afd689eb0bf2d727264898e31ff0c
Garima Jain <84665079+garimajain12@users.noreply.github.com>
2022-05-21 12:55:45 +0530

    Update main.js

--- main.js
+++ main.js
@@ -69,6 +69,7 @@ sr.reveal('.skills__img', {delay: 400})
 /*SCROLL PORTFOLIO*/
 sr.reveal('.portfolio__img', {interval: 200})
 
+
 /*SCROLL CONTACT*/
 sr.reveal('.contact__subtitle', {})
 sr.reveal('.contact__text', {interval: 200})
